b957739a2b8847f0ff4c88778c05e764250fb40e
Dave DeLong <davedelong@me.com>
2018-12-08 16:03:47 -0700

    refine 2015.4; execution goes from 3 minutes to 17 seconds

--- AOC/2015/Day 4/2015-Day4.swift
+++ AOC/2015/Day 4/2015-Day4.swift
@@ -7,17 +7,18 @@
 //
 
 extension Year2015 {
-
+    
     public class Day4: Day {
         
         public init() { super.init(inputSource: .raw("ckczppom")) }
+        
         
         override public func part1() -> String {
             var int = 1
             while true {
                 let s = "\(input.raw)\(int)"
-                let h = s.md5String()
-                if h.hasPrefix("00000") { return "\(int)" }
+                let d = s.md5()
+                if d[0] == 0 && d[1] == 0 && d[2] < 16 { return "\(int)" }
                 int += 1
             }
             fatalError("unreachable")
@@ -26,16 +27,16 @@ extension Year2015 {
         override public func part2() -> String {
             // we don't need to test 1-117946, because that happened in part 1
             // and anything that would've resulted in 6 zeroes would've satisfied part 1
-            var int = 117946
+            var int = 117947
             while true {
                 let s = "\(input.raw)\(int)"
-                let h = s.md5String()
-                if h.hasPrefix("000000") { return "\(int)" }
+                let d = s.md5()
+                if d[0] == 0 && d[1] == 0 && d[2] == 0 { return "\(int)" }
                 int += 1
             }
             fatalError("unreachable")
         }
         
     }
-
+    
 }
