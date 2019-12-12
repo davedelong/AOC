a214689cdcda344fcb78cb1952926f714f34c38f
Dave DeLong <davedelong@me.com>
2019-12-12 11:14:12 -0700

    Clean up 2019-12
    This combines the logic for part 1 and part 2 into one loop. Cycle detection is a bit more straight-forward now

--- Sources/AOC2019/Day 12/2019-Day12.swift
+++ Sources/AOC2019/Day 12/2019-Day12.swift
@@ -8,6 +8,11 @@
 
 class Day12: Day {
     
+    struct Snapshot: Hashable {
+        let p: Array<Int>
+        let v: Array<Int>
+    }
+    
     struct System {
         var positions: Array<Point3> = [Point3(x: -3, y: 15, z: -11),
                                         Point3(x: 3, y: 13, z: -19),
@@ -22,148 +27,72 @@ class Day12: Day {
                 return pE * kE
             }.sum()
         }
-    }
-    
-    struct State: Hashable {
-        let p: Vector4
-        let v: Vector4
+        
+        func snapshot(axis: Int) -> Snapshot {
+            return Snapshot(p: positions.map { $0.components[axis] },
+                            v: velocities.map { $0.components[axis] })
+        }
+        
+        mutating func step() {
+            var deltas: Array<Vector3> = [.zero, .zero, .zero, .zero]
+            
+            for pair in positions.indices.combinations(choose: 2) {
+                let m1 = positions[pair[0]]
+                let m2 = positions[pair[1]]
+                
+                for axis in 0 ..< 3 {
+                    if m1.components[axis] < m2.components[axis] {
+                        deltas[pair[0]].components[axis] += 1
+                        deltas[pair[1]].components[axis] -= 1
+                    } else if m1.components[axis] > m2.components[axis] {
+                        deltas[pair[0]].components[axis] -= 1
+                        deltas[pair[1]].components[axis] += 1
+                    }
+                }
+            }
+            
+            velocities = velocities.enumerated().map { $1 + deltas[$0] }
+            positions = positions.enumerated().map { $1 + velocities[$0] }
+        }
     }
     
     override func run() -> (String, String) {
-        return super.run()
-    }
-    
-    private func step(_ system: System) -> System {
-        var deltas: Array<Vector3> = [.zero, .zero, .zero, .zero]
-        
-        var iterator = CombinationIterator(system.positions.indices, choose: 2)
-        while let pair = iterator.next() {
-            let m1 = system.positions[pair[0]]
-            let m2 = system.positions[pair[1]]
-            
-            for axis in 0 ..< 3 {
-                if m1.components[axis] < m2.components[axis] {
-                    deltas[pair[0]].components[axis] += 1
-                    deltas[pair[1]].components[axis] -= 1
-                } else if m1.components[axis] > m2.components[axis] {
-                    deltas[pair[0]].components[axis] -= 1
-                    deltas[pair[1]].components[axis] += 1
-                }
-            }
-        }
-        
-        let newVelocities = system.velocities.enumerated().map { $1 + deltas[$0] }
-        let newPositions = system.positions.enumerated().map { $1 + newVelocities[$0] }
-        return System(positions: newPositions, velocities: newVelocities)
-    }
-    
-    override func part1() -> String {
+        var p1: String? = nil
+        var p2: String? = nil
         
         var s = System()
         
-        for _ in 0 ..< 1000 {
-            s = step(s)
-        }
+        var snapshots: Array<Set<Snapshot>> = [[], [], []]
+        var periods: Array<UInt?> = [nil, nil, nil]
         
-        return "\(s.totalEnergy)"
-    }
-    
-    override func part2() -> String {
-        
-        var positions = [
-            Vector3(x: -3, y: 15, z: -11),
-            Vector3(x: 3, y: 13, z: -19),
-            Vector3(x: -13, y: 18, z: -2),
-            Vector3(x: 6, y: 0, z: -1),
-        ]
-        
-//        positions = [
-//            Vector3(x: -8, y: -10, z: 0),
-//            Vector3(x: 5, y: 5, z: 10),
-//            Vector3(x: 2, y: -7, z: 3),
-//            Vector3(x: 9, y: -8, z: -3),
-//        ]
-        
-        var velocities = [
-            Vector3.zero,
-            Vector3.zero,
-            Vector3.zero,
-            Vector3.zero,
-        ]
-        
-        var axisPeriods: Array<UInt?> = [nil, nil, nil]
-        var seen = [
-            Set<State>(),
-            Set<State>(),
-            Set<State>(),
-        ]
-        
-        seen[0].insert(State(p: Vector4(positions.map { $0.x }), v: Vector4(positions.map { $0.x })))
-        seen[1].insert(State(p: Vector4(positions.map { $0.y }), v: Vector4(positions.map { $0.y })))
-        seen[2].insert(State(p: Vector4(positions.map { $0.z }), v: Vector4(positions.map { $0.z })))
-        
-        var steps: UInt = 0
-        repeat {
-            // first, apply gravity to velocities
-        
-            var deltas = [
-                Vector3.zero,
-                Vector3.zero,
-                Vector3.zero,
-                Vector3.zero,
-            ]
-            
-            var iterator = CombinationIterator(positions.indices, choose: 2)
-            while let pair = iterator.next() {
-                let m1 = positions[pair[0]]
-                let m2 = positions[pair[1]]
-                if m1.x < m2.x {
-                    deltas[pair[0]].x += 1
-                    deltas[pair[1]].x -= 1
-                } else if m1.x > m2.x {
-                    deltas[pair[0]].x -= 1
-                    deltas[pair[1]].x += 1
-                }
-                
-                if m1.y < m2.y {
-                    deltas[pair[0]].y += 1
-                    deltas[pair[1]].y -= 1
-                } else if m1.y > m2.y {
-                    deltas[pair[0]].y -= 1
-                    deltas[pair[1]].y += 1
-                }
-                
-                if m1.z < m2.z {
-                    deltas[pair[0]].z += 1
-                    deltas[pair[1]].z -= 1
-                } else if m1.z > m2.z {
-                    deltas[pair[0]].z -= 1
-                    deltas[pair[1]].z += 1
-                }
-            }
-            velocities = velocities.enumerated().map { $1 + deltas[$0] }
-            positions = positions.enumerated().map { $1 + velocities[$0] }
+        var stepCount: UInt = 0
+        while p1 == nil || p2 == nil {
             
             for axis in 0 ..< 3 {
-                guard axisPeriods[axis] == nil else { continue }
-                let p = Vector4(positions.map { $0.components[axis] })
-                let v = Vector4(velocities.map { $0.components[axis] })
-                let state = State(p: p, v: v)
-                if seen[axis].contains(state) {
-                    axisPeriods[axis] = steps
+                guard periods[axis] == nil else { continue }
+                let snapshot = s.snapshot(axis: axis)
+                if snapshots[axis].contains(snapshot) {
+                    periods[axis] = stepCount
                 } else {
-                    seen[axis].insert(state)
+                    snapshots[axis].insert(snapshot)
                 }
             }
             
-            steps += 1
-        } while axisPeriods.any(satisfy: { $0 == nil })
+            if stepCount == 1000 {
+                p1 = "\(s.totalEnergy)"
+            }
+            
+            if periods.allSatisfy({ $0 != nil }) {
+                let p = periods.compacted
+                p2 = "\(lcm(of: p))"
+            }
+            
+            s.step()
+            
+            stepCount += 1
+        }
         
-        // get the least common multiple of the axes' periods
-        let periods = axisPeriods.compactMap { $0 }
-        let total = lcm(of: periods)
-        
-        return "\(total)"
+        return (p1!, p2!)
     }
     
 }
