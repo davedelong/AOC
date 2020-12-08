0f5576b9f25e860ec4e84447460d7a7e48130f0a
Dave DeLong <davedelong@me.com>
2020-12-07 22:19:25 -0700

    2020 day 8

--- Sources/AOC2020/Day 8/2020-Day8.swift
+++ Sources/AOC2020/Day 8/2020-Day8.swift
@@ -7,17 +7,81 @@
 //
 
 class Day8: Day {
-
-    override func run() -> (String, String) {
-        return super.run()
+    
+    enum Instruction: Equatable {
+        case jmp(Int)
+        case acc(Int)
+        case nop(Int)
+        
+        var isAcc: Bool {
+            if case .acc = self { return true }
+            return false
+        }
     }
+    
+    private lazy var instructions: Array<Instruction> = {
+        let r: Regex = #"(jmp|acc|nop) ([-+]?\d+)"#
+        return input.rawLines.map { line -> Instruction in
+            let match = r.match(line)!
+            switch match[1]! {
+                case "jmp": return .jmp(match[int: 2]!)
+                case "acc": return .acc(match[int: 2]!)
+                default: return .nop(match[int: 2]!)
+            }
+        }
+    }()
 
     override func part1() -> String {
-        return #function
+        let (ans, _) = run(instructions)
+        return "\(ans)"
     }
 
     override func part2() -> String {
-        return #function
+        var nextIndex = 0
+        var p2 = 0
+        while nextIndex < instructions.count {
+            let slice = instructions[nextIndex...]
+            if let next = slice.firstIndex(where: { !$0.isAcc }) {
+                var copy = instructions
+                if case .jmp(let n) = copy[next] {
+                    copy[next] = .nop(n)
+                } else if case .nop(let n) = copy[next] {
+                    copy[next] = .jmp(n)
+                } else { fatalError() }
+                
+                let (ans, looped) = run(copy)
+                if looped == false {
+                    p2 = ans
+                    break
+                }
+                
+                nextIndex = next+1
+                
+            }
+        }
+        return "\(p2)"
+    }
+    
+    private func run(_ instructions: Array<Instruction>) -> (Int, Bool) {
+        var ptr = 0
+        var acc = 0
+        var visited = Set<Int>()
+        var looped = false
+        
+        while ptr < instructions.count {
+            if visited.contains(ptr) {
+                looped = true
+                break
+            }
+            visited.insert(ptr)
+            switch instructions[ptr] {
+                case .jmp(let off): ptr += off; continue
+                case .acc(let diff): acc += diff
+                case .nop: break
+            }
+            ptr += 1
+        }
+        return (acc, looped)
     }
 
 }
