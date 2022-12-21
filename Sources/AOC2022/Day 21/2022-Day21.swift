0ffb97cf3a1ed38c4f6417aa0eef9a4c10c3d7fc
Dave DeLong <davedelong@me.com>
2022-12-20 23:55:03 -0700

    major simplification

--- Sources/AOC2022/Day 21/2022-Day21.swift
+++ Sources/AOC2022/Day 21/2022-Day21.swift
@@ -12,17 +12,15 @@ class Day21: Day {
     
     static var rawInput: String? { nil }
     
-    enum Operation {
-        case value(Int)
-        case math(String, String, (Int, Int) -> Int)
-    }
-    
-    struct Monkey {
-        let name: String
-        let operation: Operation
+    enum Monkey {
+        // normally this should be an `Int`,
+        // but part 2 overflows an Int while binary searching for the answer,
+        // and also converges incorrectly due to rounding
+        case value(Double)
+        case math(String, String, (Double, Double) -> Double)
         
-        func evaluate(using monkeys: Dictionary<String, Monkey>) -> Int {
-            switch operation {
+        func evaluate(using monkeys: Dictionary<String, Monkey>) -> Double {
+            switch self {
                 case .value(let i):
                     return i
                 case .math(let a, let b, let op):
@@ -42,25 +40,25 @@ class Day21: Day {
             let name = String(line.raw.prefix(4))
             let remainder = line.raw.dropFirst(6)
             
-            let op: Operation
+            let monkey: Monkey
             if let int = line.integers.first {
-                op = .value(int)
+                monkey = .value(Double(int))
             } else if let m = r.firstMatch(in: remainder) {
                 let a = m[1]!
                 let b = m[3]!
                 
                 switch m[2] {
-                    case "+": op = .math(a, b, +)
-                    case "-": op = .math(a, b, -)
-                    case "*": op = .math(a, b, *)
-                    case "/": op = .math(a, b, /)
+                    case "+": monkey = .math(a, b, +)
+                    case "-": monkey = .math(a, b, -)
+                    case "*": monkey = .math(a, b, *)
+                    case "/": monkey = .math(a, b, /)
                     default: continue
                 }
             } else {
                 continue
             }
             
-            monkeys[name] = Monkey(name: name, operation: op)
+            monkeys[name] = monkey
         }
         
         return monkeys
@@ -69,60 +67,27 @@ class Day21: Day {
     func part1() async throws -> Part1 {
         let monkeys = parseInput()
         let root = monkeys["root"]!
-        return root.evaluate(using: monkeys)
+        return Int(root.evaluate(using: monkeys))
     }
     
     func part2() async throws -> Part2 {
-        let monkeysNamesAndExpressions = input().lines.raw.map { l -> (String, Expression) in
-            let name = String(l.prefix(4))
-            let remainder = l.dropFirst(6)
-            
-            if name == "humn" {
-                return (name, try! Expression(string: "$humn"))
-            } else {
-                var e = try! Expression(string: String(remainder))
-                if name == "root" {
-                    /*
-                     the instructions say that "root" should be an == comparison
-                     however, by doing a subtraction instead, we can instead plot the function itself
-                     and use that to converge on a correct evaluation. when it evaluates to 0,
-                     we'll have found the right value for $humn
-                     */
-                    let args = e.kind.functionArguments!
-                    e = Expression(kind: .function("subtract", args),
-                                   range: e.range)
-                }
-                return (name, e)
-            }
-            
-        }
-        
-        let monkeys = Dictionary(uniqueKeysWithValues: monkeysNamesAndExpressions)
+        var monkeys = parseInput()
         var root = monkeys["root"]!
-        
-        func resolve(_ expression: Expression) -> Expression {
-            switch expression.kind {
-                case .function(let name, let args):
-                    if args.isEmpty {
-                        let base = monkeys[name]!
-                        return resolve(base)
-                    } else {
-                        let resolved = args.map { resolve($0) }
-                        return Expression(kind: .function(name, resolved), range: expression.range)
-                    }
-                    
-                case .number: return expression
-                case .variable: return expression
-            }
+        if case .math(let a, let b, _) = root {
+            /*
+             the instructions say that "root" should be an == comparison
+             however, by doing a subtraction instead, we can instead plot the function itself
+             and use that to converge on a correct evaluation. when it evaluates to 0,
+             we'll have found the right value for $humn
+             */
+            root = .math(a, b, -)
         }
-        
-        // this just hooks up all the functions into a single, evaluatable expression
-        root = resolve(root)
         
         var range = 0 ... Int.max-1
         while range.count > 1 {
             let midPoint = range.lowerBound + (range.count / 2)
-            let answer = try! Evaluator.default.evaluate(root, substitutions: ["humn": Double(midPoint)])
+            monkeys["humn"] = .value(Double(midPoint))
+            let answer = root.evaluate(using: monkeys)
             print(midPoint, answer)
             if answer > 0 {
                 // number is too low
